9983b8c31d71e88ff48f4c675235b8df63a76bae
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-03-20 23:04:33 +0300

    result HW 14_1 develop

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -13,7 +13,7 @@ def test_product_init(first_product, second_product) -> None:
 def test_category_init(category) -> None:
     assert category.name == "Смартфоны"
     assert category.description == (
-        "Смартфоны, как средство не только коммуникации, " "но и получения дополнительных функций для удобства жизни"
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     )
     assert len(category.products) == 2
 
