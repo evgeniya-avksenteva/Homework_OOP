70f9622ff2448d7bc40b54fb35872f0faf462cc9
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-03-23 00:29:03 +0300

    homework 14_2 result

--- src/class_category.py
+++ src/class_category.py
@@ -11,7 +11,24 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
-        Category.product_count += len(self.products)
+        Category.product_count += len(self.__products)
+
+    def add_product(self, product):
+        """Добавляет новый продукт в атрибут products"""
+        self.__products.append(product)
+        self.product_count += 1
+
+    @property
+    def products(self) -> str:
+        product_list = ""
+        for product in self.__products:
+            product_list += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_list
+
+
+# if __name__ == '__main__':
+#     result = Category("Product", "Description", ["product1", "product2", "product3"])
+#     print(result)
